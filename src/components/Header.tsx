871f1acb4434331efd739cb22cff035acf5fe9a8
kobe2016LK <anasofia2382@gmail.com>
2023-06-08 14:46:46 +0800

    “中文版本”

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import  {FC, useState} from 'react'
+import {TiSocialTwitter} from "react-icons/ti";
 interface HeaderProps {
     hasSearcheBar: boolean;
 }
@@ -19,28 +20,8 @@ const Header: FC<HeaderProps> = ({ hasSearcheBar }) => {
           <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">zkSync查补</span>
         </a>
         <div className="items-center justify-between hidden w-full md:flex sm:w-auto" id="navbar-search">
-          <div className="relative mt-3 md:hidden">
-            <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-              <svg
-                className="w-5 h-5 text-gray-500"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </div>
-            <input
-              type="text"
-              id="search-navbar"
-              className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              placeholder="Search..."
-            />
+          <div className="relative mt-3">
+            <a href="https://twitter.com/Coinman_Paul" target='black'><TiSocialTwitter size="3em"/></a>
           </div>
         </div>
       </div>
